e78ec2044e533d89544f9a58cdbf70fd44e93241
Mykel Kovar <mykelkovar@mykelkovar.local>
2019-06-08 11:20:05 -0500

    commit before db setup

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Clients extends Component {
+  render() {
+    const clients = [
+      {
+        id: "123456",
+        firstName: "Jim",
+        lastName: "Halpert",
+        phone: "717123123",
+        email: "jim@dundermiff.com",
+        balance: "50"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                {" "}
+                <i className="fas fa-users" />
+                Clients
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>email</th>
+                <th>balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {/* loop through clients here */}
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>loading..</h1>;
+    }
+  }
+}
+
+export default Clients;
